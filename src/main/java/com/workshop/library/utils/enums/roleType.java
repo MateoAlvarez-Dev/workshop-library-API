aefe02e7a97e9b44a213e7d274966368b65b0437
danielsm <danielsanchezm92@gmail.com>
2024-06-21 08:11:08 -0500

    entities and relations created

---
+++ src/main/java/com/workshop/library/utils/enums/roleType.java
@@ -0,0 +1,7 @@
+package com.workshop.library.utils.enums;
+
+public enum roleType {
+    ADMIN,
+    CLIENT,
+    EMPLOYEE
+}
---
+++ src/main/java/com/workshop/library/utils/enums/statusType.java
@@ -0,0 +1,6 @@
+package com.workshop.library.utils.enums;
+
+public enum statusType {
+    ACTIVE,
+    INACTIVE
+}
